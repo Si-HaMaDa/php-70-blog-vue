e9591bf120cca385eb372ac8edd7543660b65816
Mohamed <nrookman_27@yahoo.com>
2022-08-03 18:55:01 +0200

    Add Bootstarp Icons with npm

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
-// import App from "./App.vue";
-// import Home from "./pages/Home.vue";
-// import Blog from "./pages/Blog.vue";
+import App from "./App.vue";
+import Home from "./pages/Home.vue";
+import Blog from "./pages/Blog.vue";
 import Contact from "./pages/Contact.vue";
 import Single from "./pages/Single.vue";
 
@@ -11,12 +11,14 @@ import "./assets/main.css";
 // import "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 
 import MyHeader from "./components/global/MyHeader.vue";
 import MyFooter from "./components/global/MyFooter.vue";
 import SideWidget from "./components/global/SideWidget.vue";
 
-createApp(Single)
+createApp(Home)
     .component("MyHeader", MyHeader)
     .component("SideWidget", SideWidget)
     .component("MyFooter", MyFooter)
